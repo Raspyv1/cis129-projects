e66fefc78f19d477e3ae46652bbf066d71deea44
Raspyv1 <ewinkleblack@mail.pima.edu>
2024-10-06 17:47:41 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+x = 10 
+if x > 10:
+  print('number is larger than 10')
+
+else:
+  print('number is more than 10')
+elif x > 10 and x < 7:
+  print('Number is larger than 7 but less than 10')  
